519a4cdcdc8ac0602a53d33897c2dcad7b1027d0
Omg Bebebe <omgbebebe@gmail.com>
2022-10-15 15:58:08 +0400

    separate shell.nix for flakes

---
+++ shell.flake.nix
@@ -0,0 +1,18 @@
+{ pkgs , compiler ? "ghc94" }:
+with pkgs;
+let
+  inherit (pkgs);
+  myghc = haskell.packages.${compiler}.ghcWithPackages (ps: with ps; [
+          cabal-install
+        ]);
+  libs = [ SDL2 glfw3 glm zlib pcre vulkan-loader vulkan-validation-layers vulkan-tools vulkan-tools-lunarg shaderc ];
+in
+pkgs.stdenv.mkDerivation {
+  name = "Haskan";
+  buildInputs = [ pkg-config myghc ] ++ libs;
+  shellHook = ''
+    eval $(egrep ^export ${ghc}/bin/ghc)
+    LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${lib.makeLibraryPath libs}"
+    echo "Haskan nix dev environment"
+  '';
+}
