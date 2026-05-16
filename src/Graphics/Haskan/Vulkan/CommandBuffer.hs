f05c19786538a79683322d9c3e37f3abffb65687
omgbebebe <omgbebebe@gmail.com>
2026-05-16 13:32:28 +0400

    fix: add catch-all pattern to layerTransitionAll for GENERAL layout
    Non-exhaustive case crash when transitioning storage images to GENERAL layout.
    Add fallback with ALL_COMMANDS + MEMORY_READ/WRITE barriers.

--- src/Graphics/Haskan/Vulkan/CommandBuffer.hs
+++ src/Graphics/Haskan/Vulkan/CommandBuffer.hs
@@ -218,6 +218,12 @@ layerTransitionAll commandBuffer image oldLayout newLayout layerCount = do
               Vulkan.VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
               Vulkan.VK_ACCESS_SHADER_READ_BIT
             )
+          _ ->
+            ( Vulkan.VK_PIPELINE_STAGE_ALL_COMMANDS_BIT,
+              Vulkan.VK_ACCESS_MEMORY_READ_BIT .|. Vulkan.VK_ACCESS_MEMORY_WRITE_BIT,
+              Vulkan.VK_PIPELINE_STAGE_ALL_COMMANDS_BIT,
+              Vulkan.VK_ACCESS_MEMORY_READ_BIT .|. Vulkan.VK_ACCESS_MEMORY_WRITE_BIT
+            )
 
       subresourceRange =
         Vulkan.createVk
