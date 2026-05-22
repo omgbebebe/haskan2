8f42f981e3a74caf450f7b0bbee4f06bf35d058a
omgbebebe <omgbebebe@gmail.com>
2026-05-22 20:54:27 +0400

    cleanup: delete 3 orphaned legacy modules
    Remove source files that are not in haskan2.cabal and have no importers:
    - Graphics.Haskan.Engine.Core: superseded by Engine.Types
    - Graphics.Haskan.Noise: superseded by GPU compute shaders
    - Graphics.Haskan.Events: unused 8-line SDL wrapper
    
    Milestone modules preserved (Bindless, Specialization, Compute.Test).

--- src/Graphics/Haskan/Events.hs
+++
@@ -1,8 +0,0 @@
-module Graphics.Haskan.Events where
-
-import Graphics.Haskan.Resources (MonadManaged, alloc_)
-import SDL qualified
-
-managedEvents :: (MonadManaged m) => m ()
-managedEvents =
-  alloc_ "SDL Events" (SDL.initialize @[] [SDL.InitEvents]) SDL.quit
